4d45e7f6f8b9fa233460a433317a74c6ec615698
Jeroen Hoegen Dijkhof <git@jeroenhd.nl>
2016-09-23 15:53:43 +0200

    Fix project inclusion, json files were excluded by accident

--- stationdata-converter/Station Data Converter/src/Station Data Converter/Program.cs
+++ stationdata-converter/Station Data Converter/src/Station Data Converter/Program.cs
@@ -7,15 +7,42 @@ namespace Station_Data_Converter
 {
     public class Program
     {
+        private static String loadFile(String filename)
+        {
+            var filestream = System.IO.File.OpenRead(filename);
+            var reader = new System.IO.StreamReader(filestream);
+            String jsonData = reader.ReadToEnd();
+
+            reader.Dispose();
+            filestream.Dispose();
+
+            return jsonData;
+        }
+
         public static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args.Length < 2)
             {
-                Console.Error.WriteLine("Please pass the path to the JSON file as a command line argument");
+                Console.Error.WriteLine("Please pass the path to the JSON file and the path to the output file as a command line argument");
                 return;
             }
 
-            Console.ReadLine();
+            String fileNameInput = args[0];
+            String fileNameOutput = args[1];
+
+            String data;
+            try
+            {
+                data = loadFile(fileNameInput);
+            } catch(System.IO.IOException e)
+            {
+                Console.Error.WriteLine("Failed to read JSON data: {0}", e.Message);
+                return;
+            }
+
+            Console.WriteLine("Loaded JSON data ({0} bytes)", data.Length);
+
+            
         }
     }
 }
